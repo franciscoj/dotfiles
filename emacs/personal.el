328e7c59d51b673c5c9afad247c7cde145602b1d
Fran Casas <nflamel@otrobloggeek.com>
2014-03-08 18:59:09 +0100

    That works better for jabber :D

--- emacs/personal.el
+++ emacs/personal.el
@@ -19,10 +19,40 @@
 (setq rspec-use-rake-when-possible nil)
 
 (prelude-require-package 'jabber)
+
+;; simpler format for prompts
+(setq jabber-chat-buffer-show-avatar nil
+      jabber-chat-foreign-prompt-format "> "
+      jabber-chat-local-prompt-format "> "
+      jabber-chat-system-prompt-format "*** "
+      jabber-chat-time-format "%H:%M"
+      jabber-default-show ""
+      jabber-groupchat-prompt-format "%n> "
+      jabber-muc-private-foreign-prompt-format "%g/%n> ")
+
+;; improve the default settings
+(setq jabber-roster-line-format "%c %-25n %u %-8s"
+      jabber-roster-show-title nil
+      jabber-show-resources nil
+      jabber-show-offline-contacts nil
+      jabber-avatar-cache-directory "/tmp/jabber-avatars"
+      jabber-events-request-these (quote (delivered displayed composing)))
+
+;; don't show any presence notifications
+(setq jabber-alert-presence-hooks '())
+
+;; history
+(setq jabber-history-enabled t
+      jabber-use-global-history nil
+      jabber-backlog-number 40
+      jabber-backlog-days 30
+      jabber-history-dir "~/.cache/jabber-history")
+
 (setq jabber-account-list
-      '(("nflamel@gmail.com"
-         (:password . nil)
+      `((,(read-from-minibuffer "Gtalk username: ")
+         (:password . ,(read-passwd "Gtalk password: "))
          (:network-server . "talk.google.com")
+         (:port . 443)
          (:connection-type . ssl))))
 
 (setq projectile-switch-project-action 'projectile-dired)
