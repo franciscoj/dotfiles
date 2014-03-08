a8b9f39cc0903bd2c6c92e429f885a72e0e17702
Fran Casas <nflamel@otrobloggeek.com>
2014-03-08 13:29:00 +0100

    Don't log on tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,4 +22,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+  config.before(:all) do
+    Anvil.module_eval { @logger = Logger.new(nil) }
+  end
 end
