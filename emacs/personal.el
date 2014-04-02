2e35f2df232bfbc88ecadabaa81845750c53814e
Fran Casas <nflamel@otrobloggeek.com>
2014-04-02 13:22:52 +0200

    Add indent guide and multiple cursors

--- emacs/personal.el
+++ emacs/personal.el
@@ -71,4 +71,11 @@
 (setq display-time-format "%R %d/%m/%Y")
 
 (prelude-require-package 'indent-guide)
+(require 'indent-guide)
 (indent-guide-global-mode)
+
+(prelude-require-package 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
