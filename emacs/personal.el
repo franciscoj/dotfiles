594759fce9594c9a4d8de382fb23365e2401b06c
Fran Casas <nflamel@otrobloggeek.com>
2014-04-17 14:40:58 +0200

    Remove multiple cursors since I don't use it

--- emacs/personal.el
+++ emacs/personal.el
@@ -76,9 +76,3 @@
 (prelude-require-package 'indent-guide)
 (require 'indent-guide)
 (indent-guide-global-mode)
-
-(prelude-require-package 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
