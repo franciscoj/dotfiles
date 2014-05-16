d921e3730cbdbceee3cd86f80a52afc3bf5aad34
Fran Casas <nflamel@otrobloggeek.com>
2014-05-16 09:45:35 +0200

    Set rbenv folder on mac

--- emacs/personal.el
+++ emacs/personal.el
@@ -4,12 +4,13 @@
 (prelude-require-package 'ruby-tools)
 
 (prelude-require-package 'markdown-mode)
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (prelude-require-package 'rbenv)
+(if (eq system-type 'darwin)
+    (setq rbenv-installation-dir "/usr/local/Cellar/rbenv/0.4.0"))
 (global-rbenv-mode)
 
 (prelude-require-package 'yard-mode)
