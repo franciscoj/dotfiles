4e0136219b7fa1b0f54430109b5301bb50375c43
Fran Casas <nflamel@gmail.com>
2016-08-12 08:18:59 +0200

    Add vid2gif script

---
+++ bin/vid2gif.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Extracted from: http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html#usage
+
+palette="/tmp/palette.png"
+
+filters="fps=15,scale=640:-1:flags=lanczos"
+
+ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
