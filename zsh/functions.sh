fedfa8ce2bdeb195171f01692a97369fd3ef79d0
Fran Casas <fran.casas@redbooth.com>
2016-12-07 13:11:41 +0100

    Simpler zsh config

--- zsh/functions.sh
+++
@@ -1,8 +0,0 @@
-function dot_files_load() {
-    # Platform depending exports and sources
-    if [[ $dot_files_platform == 'Linux' ]]; then
-        source $dot_files_base/$1.linux
-    elif [[ $dot_files_platform == 'Darwin' ]]; then
-        source $dot_files_base/$1.mac
-    fi
-}
