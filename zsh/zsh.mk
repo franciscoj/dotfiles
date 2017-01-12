3063cdc87cdc00bb12ce788866cd509433d00bcc
Fran Casas <nflamel@gmail.com>
2017-01-12 13:02:08 +0100

    Install oh-my-zsh

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -8,16 +8,20 @@ ZSHRC_SRC     := $(DOTFILES)/zsh/zshrc
 ZSHRC         := $(DST_DIR)/.zshrc
 ZPROFILE_SRC  := $(DOTFILES)/zsh/zprofile
 ZPROFILE      := $(DST_DIR)/.zprofile
+OH_MY_ZSH_URL := https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE)
+zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
 
 $(ZPROFILE):
 	$(LINK) $(ZPROFILE_SRC) $@
+
+$(OH_MY_ZSH):
+	curl -fsSL $(OH_MY_ZSH_URL)
 
 clean_zsh: banner_clean_zsh
 	$(RM) $(ZSHRC)
