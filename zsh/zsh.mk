09cc6f2238afc772f751a7649a6741c81bd686a7
Fran Casas <nflamel@gmail.com>
2017-01-12 13:04:43 +0100

    Proper target for oh-my-zsh

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -9,10 +9,11 @@ ZSHRC         := $(DST_DIR)/.zshrc
 ZPROFILE_SRC  := $(DOTFILES)/zsh/zprofile
 ZPROFILE      := $(DST_DIR)/.zprofile
 OH_MY_ZSH_URL := https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+OH-MY_ZSH_DST := $(DST_DIR)/.oh-my-zsh
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH)
+zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
@@ -20,12 +21,13 @@ $(ZSHRC):
 $(ZPROFILE):
 	$(LINK) $(ZPROFILE_SRC) $@
 
-$(OH_MY_ZSH):
+$(OH_MY_ZSH_DST):
 	curl -fsSL $(OH_MY_ZSH_URL)
 
 clean_zsh: banner_clean_zsh
 	$(RM) $(ZSHRC)
 	$(RM) $(ZPROFILE)
+	$(RM) $(OH_MY_ZSH_DST)
 
 endif
 
