d97a63526254d267d28fb2a8c50e29b37973d91a
Fran Casas <nflamel@gmail.com>
2017-11-25 13:41:43 +0100

    Set base16-monokai as default terminal colors

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -4,22 +4,27 @@ ifdef ZSH
 INSTALLERS += zsh
 CLEANERS   += clean_zsh
 
-ZSHRC_SRC     := $(DOTFILES)/zsh/zshrc
-ZSHRC         := $(DST_DIR)/.zshrc
-ZPROFILE_SRC  := $(DOTFILES)/zsh/zprofile
-ZPROFILE      := $(DST_DIR)/.zprofile
-OH_MY_ZSH_URL := git://github.com/robbyrussell/oh-my-zsh.git
-OH_MY_ZSH_DST := $(DST_DIR)/.oh-my-zsh
+ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
+ZSHRC             := $(DST_DIR)/.zshrc
+ZPROFILE_SRC      := $(DOTFILES)/zsh/zprofile
+ZPROFILE          := $(DST_DIR)/.zprofile
+OH_MY_ZSH_URL     := git://github.com/robbyrussell/oh-my-zsh.git
+OH_MY_ZSH_DST     := $(DST_DIR)/.oh-my-zsh
+BASE16_COLORS_DST := $(CONFIG_DIR)/base16-shell
+BASE16_COLORS_URL := https://github.com/chriskempson/base16-shell.git
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST)
+zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
 
 $(ZPROFILE):
 	$(LINK) $(ZPROFILE_SRC) $@
+
+$(BASE16_COLORS_DST):
+	$(CLONE) $(BASE16_COLORS_URL) $@
 
 $(OH_MY_ZSH_DST):
 	$(CLONE) $(OH_MY_ZSH_URL) $@
