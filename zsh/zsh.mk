47195c605e6cf25aab904fc00e8c57d66f387404
Fran Casas <nflamel@gmail.com>
2018-10-28 16:16:46 +0100

    use right zsh config for arch linux

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -6,8 +6,8 @@ CLEANERS   += clean_zsh
 
 ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
 ZSHRC             := $(DST_DIR)/.zshrc
-ZPROFILE_SRC      := $(DOTFILES)/zsh/zprofile
-ZPROFILE          := $(DST_DIR)/.zprofile
+ZSHENV_SRC        := $(DOTFILES)/zsh/zshenv
+ZSHENV            := $(DST_DIR)/.zshenv
 OH_MY_ZSH_URL     := git://github.com/robbyrussell/oh-my-zsh.git
 OH_MY_ZSH_DST     := $(DST_DIR)/.oh-my-zsh
 BASE16_COLORS_DST := $(CONFIG_DIR)/base16-shell
@@ -15,13 +15,13 @@ BASE16_COLORS_URL := https://github.com/chriskempson/base16-shell.git
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
+zsh: banner_install_zsh $(ZSHRC) $(ZSHENV) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
 
-$(ZPROFILE):
-	$(LINK) $(ZPROFILE_SRC) $@
+$(ZSHENV):
+	$(LINK) $(ZSHENV_SRC) $@
 
 $(BASE16_COLORS_DST):
 	$(CLONE) $(BASE16_COLORS_URL) $@
@@ -31,7 +31,7 @@ $(OH_MY_ZSH_DST):
 
 clean_zsh: banner_clean_zsh
 	$(RM) $(ZSHRC)
-	$(RM) $(ZPROFILE)
+	$(RM) $(ZSHENV)
 	$(RM) $(OH_MY_ZSH_DST)
 
 endif
