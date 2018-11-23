1f1a6e6b6378bb9349a5f5b3da3173e3a0951443
Fran Casas <nflamel@gmail.com>
2018-11-23 20:44:18 +0100

    zprofile/zenv depending on mac/linux

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -4,24 +4,36 @@ ifdef ZSH
 INSTALLERS += zsh
 CLEANERS   += clean_zsh
 
-ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
-ZSHRC             := $(DST_DIR)/.zshrc
-ZSHENV_SRC        := $(DOTFILES)/zsh/zshenv
-ZSHENV            := $(DST_DIR)/.zshenv
+ZSHRC_SRC  := $(DOTFILES)/zsh/zshrc
+ZSHRC      := $(DST_DIR)/.zshrc
+ZSHENV_SRC := $(DOTFILES)/zsh/zshenv
+ZSHENV     := $(DST_DIR)/.zshenv
+ZPROFILE   := $(DST_DIR)/.zprofile
 
 .PHONY: zsh clean_zsh
 
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Darwin)
+zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE)
+$(ZPROFILE):
+	$(LINK) $(ZSHENV_SRC) $@
+else
 zsh: banner_install_zsh $(ZSHRC) $(ZSHENV)
+$(ZSHENV):
+	$(LINK) $(ZSHENV_SRC) $@
+endif
+
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
 
-$(ZSHENV):
-	$(LINK) $(ZSHENV_SRC) $@
-
 clean_zsh: banner_clean_zsh
 	$(RM) $(ZSHRC)
+ifeq ($(UNAME_S),Darwin)
+	$(RM) $(ZPROFILE)
+else
 	$(RM) $(ZSHENV)
+endif
 
 endif
 
