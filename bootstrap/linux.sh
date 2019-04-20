32f7e2a4d24c44b220cb6fca1d742791de8f9f3f
Fran Casas <nflamel@gmail.com>
2019-04-20 20:05:05 +0200

    doc: bootstrap

--- bootstrap/linux.sh
+++ bootstrap/linux.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
+# Install yay
 ./bin/install/yay.sh
 
+# Install base system packages
 yay -S --needed - < arch_packages_list
 
+# Install custom system configuration
 cd system
 makepkg -di
 cd ..
