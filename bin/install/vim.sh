9ab05dcd220ef74d6446a5f1ce43569fcd29730e
Fran Casas <nflamel@gmail.com>
2019-11-04 19:03:07 +0100

    refactor(dotbot): vim install script

--- bin/install/vim.sh
+++ bin/install/vim.sh
@@ -3,11 +3,11 @@
 set -ex
 
 plug_url=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-plug_dst=~/.config/nvim/autoload/plug.vim
+plug_dst=$HOME/.config/nvim/autoload/plug.vim
 
 if [ ! -f "$plug_dst" ]; then
   curl -fLo $plug_dst $plug_url
   nvim +PlugInstall +qa
 
-  mkdir -p ~/.config/nvim/files/backup
+  mkdir -p $HOME/.config/nvim/files/backup
 fi
