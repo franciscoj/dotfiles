7db2a4af678cbea0c6050f02a62ac39f9a1eb6da
Fran Casas <nflamel@gmail.com>
2019-11-18 12:29:12 +0100

    feat(alias): add be for bundle exec

--- zsh/alias/init.zsh
+++ zsh/alias/init.zsh
@@ -1,9 +1,9 @@
 # vim: set ft=sh
 
-alias _='sudo'
-alias rake='noglob rake'
+alias _="sudo"
+alias rake="noglob rake"
 
-alias e='${(z)VISUAL:-${(z)EDITOR}}'
+alias e="${(z)VISUAL:-${(z)EDITOR}}"
 alias vi=$EDITOR
 
 alias tmux="tmux -u"
@@ -12,3 +12,4 @@ alias ll="exa -lh"
 alias la="exa -lha"
 alias lg="exa -lha --git"
 
+alias be="bundle exec"
