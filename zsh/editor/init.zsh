570e9fd55603977af4b89417643335d3e22e1edf
Fran Casas <nflamel@gmail.com>
2020-05-12 07:26:55 +0200

    fix: vim to edit lines on the shell

--- zsh/editor/init.zsh
+++ zsh/editor/init.zsh
@@ -6,3 +6,7 @@ export VISUAL=$EDITOR
 alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias vi='$EDITOR'
 
+# Edit lines on the shell with vim
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
