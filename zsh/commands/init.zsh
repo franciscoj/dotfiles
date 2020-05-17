1ba0c03791d9bda43a5637ef9e7a7105a96f164f
Fran Casas <nflamel@gmail.com>
2020-05-17 18:34:50 +0200

    fix: disable some stuff that was breaking zshrc

--- zsh/commands/init.zsh
+++ zsh/commands/init.zsh
@@ -2,9 +2,9 @@
 [[ -x "$(command -v hub)" ]] && eval "$(hub alias -s)"
 
 # Load asdf completions if they exist
-[[ -d "$HOME/.asdf" ]] && source $HOME/.asdf/completions/asdf.bash
+# This is now broken if you use zsh :shrug:
+# [[ -d "$HOME/.asdf" ]] && source $HOME/.asdf/completions/asdf.bash
 
 # Load fzf into zsh
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
