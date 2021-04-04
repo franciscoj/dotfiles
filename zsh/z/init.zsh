ce7fc90b7baee499d108cf3bc4e8b703049edb2c
Fran Casas <nflamel@gmail.com>
2021-04-04 20:45:08 +0200

    refactor: improve `z` config
    - Make it work on `j` instead of `z`
    - Make it work on mac in addition to linux

--- zsh/z/init.zsh
+++ zsh/z/init.zsh
@@ -1,1 +1,8 @@
+# Use `j` for the command instead of `z`
+export _Z_CMD="j"
+
+# On linux
 [[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh
+
+# On mac with homebrew
+[[ -r "/usr/local/etc/profile.d/z.sh" ]] && source /usr/local/etc/profile.d/z.sh
