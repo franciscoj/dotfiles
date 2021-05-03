cfcdcba5e94d74314d1dfb49bdd415e0a62176ed
Fran Casas <nflamel@gmail.com>
2021-05-03 22:39:05 +0200

    feat: CTRL-X E to edit the current line on $EDITOR

--- zsh/editor/init.zsh
+++ zsh/editor/init.zsh
@@ -7,5 +7,9 @@ export VISUAL=$EDITOR
 alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias vi='$EDITOR'
 
-# But still use emacs-style zsh bindings
+# Use emacs-style zsh bindings
 bindkey -e
+# `CTRL-X E` to edit the current line with $EDITOR
+autoload edit-command-line
+zle -N edit-command-line
+bindkey '^Xe' edit-command-line
