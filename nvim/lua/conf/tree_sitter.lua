9deb26014176d4dca1a7ce0c0c28f0361a23eb38
Fran Casas <nflamel@gmail.com>
2021-05-24 09:17:48 +0200

    feat(vim): improve trees sitter config a bit

--- nvim/lua/conf/tree_sitter.lua
+++ nvim/lua/conf/tree_sitter.lua
@@ -20,6 +20,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = { enable = true },
   incremental_selection = { enable = true },
   textobjects = { enable = true },
-  indent = { enable = true }
+  indent = { enable = true },
+  folding = { enable = false }
 }
 
