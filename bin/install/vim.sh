ff52890c8ae5975b5a90b8dfca38ad08efc4789a
Fran Casas <nflamel@gmail.com>
2021-06-17 11:49:46 +0200

    feat: install neovim appimage

--- bin/install/vim.sh
+++ bin/install/vim.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+if [ -n "$CODESPACES" ]; then
+  cd ~/
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x nvim.appimage
+  ln -sf nvim.appimage /usr/bin/nvim
+  cd -
+fi
+
 plug_url=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 plug_dst=$HOME/.config/nvim/autoload/plug.vim
 
