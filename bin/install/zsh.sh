6450f8c0f037b0ed0facca18e0115edb3c5be6a8
Fran Casas <nflamel@gmail.com>
2021-06-17 17:31:07 +0200

    fix: improve zplug settings

--- bin/install/zsh.sh
+++ bin/install/zsh.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 set -ex
 
-zplug_home=$HOME/.config/zplug
-zplug_git=https://github.com/zplug/zplug
+# Don't install zplug on codespaces
+if [[ -f /workspaces/.codespaces/.persistedshare/dotfiles/README.md ]]; then
+  base16_home=$HOME/.config/base16-shell
+  base16_git=https://github.com/chriskempson/base16-shell.git
 
-if [ ! -d "$zplug_home" ]; then 
-  git clone $zplug_git $zplug_home
-fi
+  if [ ! -d "$base16_home" ]; then
+    git clone $base16_git $base16_home
+  fi
+else
+  zplug_home=$HOME/.config/zplug
+  zplug_git=https://github.com/zplug/zplug
 
-base16_home=$HOME/.config/base16-shell
-base16_git=https://github.com/chriskempson/base16-shell.git
+  if [ ! -d "$zplug_home" ]; then 
+    git clone $zplug_git $zplug_home
+  fi
 
-if [ ! -d "$base16_home" ]; then
-  git clone $base16_git $base16_home
+  base16_home=$HOME/.config/base16-shell
+  base16_git=https://github.com/chriskempson/base16-shell.git
+
+  if [ ! -d "$base16_home" ]; then
+    git clone $base16_git $base16_home
+  fi
 fi
