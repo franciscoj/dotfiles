e548dedb4ac62b51927e5184fb9d8e7a7cb4af5e
Fran Casas <nflamel@gmail.com>
2021-06-17 11:36:21 +0200

    fix: tmux installation

--- bin/install/tmux.sh
+++ bin/install/tmux.sh
@@ -2,7 +2,7 @@
 set -ex
 
 tpm_git=https://github.com/tmux-plugins/tpm
-tpm_home=$HOME/.config/tmux/plugins/tpm
+tpm_home=$HOME/.tmux/plugins/tpm
 
 if [ ! -d "$tpm_home" ]; then
   git clone $tpm_git $tpm_home
