240f2888b4090b6ccb02c0a2781798f8765b0703
Fran Casas <nflamel@gmail.com>
2021-06-17 17:15:20 +0200

    fix: zplug

--- zsh/codespaces/init.zsh
+++ zsh/codespaces/init.zsh
@@ -1,3 +1,5 @@
-if [ -n "$CODESPACES" ]; then
+if [[ -f /workspaces/.codespaces/.persistedshare/dotfiles/README.md ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  export ZPLUG_HOME=/home/linuxbrew/.linuxbrew/opt/zplug
+  source $ZPLUG_HOME/init.zsh
 fi
