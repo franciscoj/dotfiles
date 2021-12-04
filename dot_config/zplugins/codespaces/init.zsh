a83dddca6a720d2d00b011c49274387dad4d392a
Fran Casas <nflamel@gmail.com>
2021-12-04 09:27:13 +0100

    fix: simiplify a bit by making it a bit more generic

--- dot_config/zplugins/codespaces/init.zsh
+++ dot_config/zplugins/codespaces/init.zsh
@@ -1,3 +1,2 @@
-if [ -v CODESPACES ]; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
+# Load linuxbrew if it exists
+[[ -f /home/linuxbrew/.linuxbrew/bin/brew ]] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
--- install.sh
+++ install.sh
@@ -2,28 +2,22 @@
 
 set -e # -e: exit on error
 
-if [[ -z "${CODESPACES}" ]]; then
-  echo "not codespaces"
-else
-  apt install -y ripgrep
-
-  if [ ! "$(command -v chezmoi)" ]; then
-    bin_dir="$HOME/.local/bin"
-    chezmoi="$bin_dir/chezmoi"
-    if [ "$(command -v curl)" ]; then
-      sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-    elif [ "$(command -v wget)" ]; then
-      sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-    else
-      echo "To install chezmoi, you must have curl or wget installed." >&2
-      exit 1
-    fi
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsLS https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
   else
-    chezmoi=chezmoi
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
   fi
-
-  # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-  script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-  # exec: replace current process with chezmoi init
-  exec "$chezmoi" init --apply "--source=$script_dir"
+else
+  chezmoi=chezmoi
 fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
