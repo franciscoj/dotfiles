7bda77e93f21576e986712d89bcd1ebd094b4255
Fran Casas <nflamel@gmail.com>
2021-12-04 08:41:39 +0000

    fix: try install from inside codespaces

--- install.sh
+++ install.sh
@@ -2,19 +2,25 @@
 
 set -e # -e: exit on error
 
-if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsLS https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+if [ -z "${CODESPACES}" ]; then
+  echo "not codespaces"
 else
-  chezmoi=chezmoi
+  sudo apt install -y ripgrep
+
+  if [ ! "$(command -v chezmoi)" ]; then
+    bin_dir="$HOME/.local/bin"
+    chezmoi="$bin_dir/chezmoi"
+    if [ "$(command -v curl)" ]; then
+      sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+    elif [ "$(command -v wget)" ]; then
+      sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+    else
+      echo "To install chezmoi, you must have curl or wget installed." >&2
+      exit 1
+    fi
+  else
+    chezmoi=chezmoi
+  fi
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
