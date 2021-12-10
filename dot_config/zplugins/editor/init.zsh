aa292387d1a9ebf3c164f2785c4c0a107e1ba79f
Fran Casas <nflamel@gmail.com>
2021-12-10 23:05:17 +0100

    feat: prepare editor cfg for lvim

--- dot_config/zplugins/editor/init.zsh
+++ dot_config/zplugins/editor/init.zsh
@@ -1,11 +1,6 @@
-export EDITOR='nvim'
-export VISUAL='code -w'
-
-export DIFFPROG='nvim -d'
-export MANPAGER='nvim +Man!'
-
 alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias vi='$EDITOR'
+alias nvim='lvim'
 
 # Use emacs-style zsh bindings
 bindkey -e
@@ -13,3 +8,10 @@ bindkey -e
 autoload edit-command-line
 zle -N edit-command-line
 bindkey '^Xe' edit-command-line
+
+export EDITOR='lvim'
+export VISUAL='lvim'
+
+export DIFFPROG='nvim -d'
+export MANPAGER='nvim +Man!'
+
