7060557a8e84cdd3c3981f6412a9ae3cdc4fdc0b
Fran Casas <nflamel@gmail.com>
2021-12-12 11:23:52 +0100

    feat: install lunarvim

---
+++ run_once_before_install-lunarvim.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Installing: LunarVim ********************"
+curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | /bin/bash /dev/stdin --no-install-dependencies
