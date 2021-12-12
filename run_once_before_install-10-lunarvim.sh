354e34f927f0e208c8fe067d94e9027fc6f171f3
Fran Casas <nflamel@gmail.com>
2021-12-12 14:23:28 +0100

    fix: stop on error

--- run_once_before_install-10-lunarvim.sh
+++ run_once_before_install-10-lunarvim.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+set -e
+
 echo "Installing: LunarVim ********************"
 curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | /bin/bash /dev/stdin --no-install-dependencies
