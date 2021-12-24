7cae13459c6af925217f6e75d135f032f9561c2d
Fran Casas <nflamel@gmail.com>
2021-12-24 14:28:10 +0100

    refactor: remove lunarvim config

--- dot_config/zplugins/editor/init.zsh
+++ dot_config/zplugins/editor/init.zsh
@@ -1,6 +1,5 @@
 alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias vi='$EDITOR'
-alias nvim='lvim'
 
 # Use emacs-style zsh bindings
 bindkey -e
@@ -9,8 +8,8 @@ autoload edit-command-line
 zle -N edit-command-line
 bindkey '^Xe' edit-command-line
 
-export EDITOR='lvim'
-export VISUAL='lvim'
+export EDITOR='nvim'
+export VISUAL='nvim'
 
 export DIFFPROG='nvim -d'
 export MANPAGER='nvim +Man!'
