14cfec596953bfdcf54f92c64555d24e8fddff25
Fran Casas <nflamel@gmail.com>
2021-12-24 14:28:51 +0100

    feat: add telescope

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,7 +9,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
-require('packer').startup(function(use)
+local packer_plugins = function(use)
   use "wbthomason/packer.nvim"
   use "tpope/vim-abolish"
   use "tpope/vim-characterize"
@@ -27,8 +27,25 @@ require('packer').startup(function(use)
     require = "williamboman/nvim-lsp-installer",
     config = function() require("conf-lsp") end,
   }
+  use {
+    "nvim-telescope/telescope.nvim",
+    config = function() require("conf-telescope") end,
+    requires = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+    },
+  }
 
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
-end)
+end
+
+local packer_config = {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}
+require('packer').startup({packer_plugins, config = packer_config})
---
+++ dot_config/nvim/lua/conf-telescope.lua
@@ -0,0 +1,37 @@
+local h = require("h")
+
+h.nnoremap("<leader>f", ":Telescope find_files<cr>")
+h.nnoremap("<leader>s", ":Telescope live_grep<cr>")
+h.nnoremap("<leader>F", ":Telescope resume<cr>")
+
+local action_layout = require("telescope.actions.layout")
+local telescope = require("telescope")
+telescope.setup({
+  defaults = {
+    mappings = {
+      n = {
+        ["<M-p>"] = action_layout.toggle_preview
+      },
+      i = {
+        ["<M-p>"] = action_layout.toggle_preview
+      },
+    },
+    layout_config = {
+      width = 0.9,
+      height = 0.9,
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+  -- other configuration values here
+})
+telescope.load_extension("fzf")
