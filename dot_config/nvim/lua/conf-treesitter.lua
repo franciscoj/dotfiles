f7608da3cacca4b28332e92fca293ee5d96bbb8b
Fran Casas <nflamel@gmail.com>
2021-12-26 09:59:53 +0100

    feat: enable incremental selection on treesitter

--- dot_config/nvim/lua/conf-treesitter.lua
+++ dot_config/nvim/lua/conf-treesitter.lua
@@ -19,4 +19,6 @@ require'nvim-treesitter.configs'.setup {
   },
   highlight = { enable = true },
   indent = { enable = true },
+  incremental_selection = { enable = true },
+  textobjects = { enable = true },
 }
