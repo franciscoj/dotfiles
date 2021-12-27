8134c298822f1c617300afbb11dacd9cd6b131f7
Fran Casas <nflamel@gmail.com>
2021-12-27 20:53:53 +0100

    refactor: organize plugin loading code a bit

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,15 +1,16 @@
 require("options") -- Load the base defaults
 require("keymaps") -- Global key mappings that don't deppend on plugins
 
-local fn = vim.fn
 
 -- Install packer if it isn't installed
+local fn = vim.fn
 local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
-local packer_plugins = function(use)
+local packer = require("packer")
+local plugins = function(use)
   use "wbthomason/packer.nvim"
 
   -- QOL plugins
@@ -29,6 +30,7 @@ local packer_plugins = function(use)
       vim.g.splitjoin_ruby_hanging_args = false
     end
   }
+
   -- Git
   use {
     "tpope/vim-fugitive",
@@ -39,6 +41,7 @@ local packer_plugins = function(use)
     },
     config = function() require("conf-git") end,
   }
+
   -- Support project specific config
   use {
     "embear/vim-localvimrc",
@@ -46,10 +49,12 @@ local packer_plugins = function(use)
       vim.g.localvimrc_persistent = 1
     end
   }
+
   -- Look & Feel
   use "kyazdani42/nvim-web-devicons"
   use { "dracula/vim", config = "vim.cmd[[colorscheme dracula]]" }
   use { 'nvim-lualine/lualine.nvim', config = function() require("conf-statusline") end }
+
   -- Neovim/treesitter/LSP specific
   use { "nvim-treesitter/nvim-treesitter", config = function() require("conf-treesitter") end }
   use {
@@ -65,6 +70,7 @@ local packer_plugins = function(use)
       { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
     },
   }
+
   -- Autocomplete + snippets
   use {
     "hrsh7th/nvim-cmp",
@@ -80,6 +86,7 @@ local packer_plugins = function(use)
     },
     config = function () require("conf-cmp") end
   }
+
   -- Running tests
   use { "kassio/neoterm", config = function() require("conf-neoterm") end }
   use {
@@ -88,15 +95,16 @@ local packer_plugins = function(use)
   }
 
   if PACKER_BOOTSTRAP then
-    require("packer").sync()
+    packer.sync()
   end
 end
 
-local packer_config = {
+local config = {
   display = {
     open_fn = function()
       return require('packer.util').float({ border = 'single' })
     end
   }
 }
-require('packer').startup({packer_plugins, config = packer_config})
+
+packer.startup({plugins, config = config})
