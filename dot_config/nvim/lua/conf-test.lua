cbac0702147361e08c4d299ecd93d0cd3e14ab2a
Fran Casas <nflamel@gmail.com>
2022-01-11 22:12:49 +0100

    fix(nvim): run nearest test shortcut

--- dot_config/nvim/lua/conf-test.lua
+++ dot_config/nvim/lua/conf-test.lua
@@ -3,6 +3,6 @@ local h = require("h")
 vim.g["test#strategy"] = "neoterm"
 
 h.nnoremap("<leader>tf", ":TestFile<cr>")
-h.nnoremap("<leader>tl", ":TesttNearest<cr>")
+h.nnoremap("<leader>tl", ":TestNearest<cr>")
 h.nnoremap("<leader>tr", ":TestLast<cr>")
 h.nnoremap("<leader>ts", ":TestSuite<cr>")
