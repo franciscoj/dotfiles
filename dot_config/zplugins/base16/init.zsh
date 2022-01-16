33fad0579ac4cbbc5d2ca45d62584c742d666f3b
Fran Casas <nflamel@gmail.com>
2022-01-16 20:43:21 +0100

    feat(term): remove base16

--- dot_config/zplugins/base16/init.zsh
+++
@@ -1,8 +0,0 @@
-# Use base16 to setup terminal colors everywhere.
-
-BASE16_SCHEME="dracula"
-BASE16_SHELL="$HOME/.config/base16-shell/scripts/base16-$BASE16_SCHEME.sh"
-if [[ -s $BASE16_SHELL ]]; then
-  . $BASE16_SHELL
-fi
-
