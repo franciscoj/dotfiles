8c86e0a70e575345bae96f44d1241d92e9fd3b46
Fran Casas <nflamel@gmail.com>
2022-01-22 21:01:20 +0100

    feat(nvim): GBrowse mapping and gitcommit ftplugin

--- dot_config/nvim/lua/conf-git.lua
+++ dot_config/nvim/lua/conf-git.lua
@@ -5,12 +5,5 @@ h.nnoremap("<leader>gs", ":Git<cr>")
 h.nnoremap("<leader>gb", ":Git blame<cr>")
 h.nnoremap("<leader>gl", ":GV!<cr>") -- Log for current file
 h.nnoremap("<leader>gL", ":GV<cr>") -- Log for repo
-
--- Add spell check to git commits
-vim.cmd([[
-augroup git_commits
-  autocmd!
-
-  autocmd FileType gitcommit setlocal spell spelllang=en_us
-augroup END
-]])
+h.nnoremap("<leader>go", ":GBrowse<CR>")
+h.xnoremap("<leader>go", ":'<,'>GBrowse<CR>")
