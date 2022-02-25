c3222ca12c172be89a0897d431510bc0ae739479
Fran Casas <nflamel@gmail.com>
2022-02-25 11:11:58 +0100

    fix: chezmoi install

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ else
     bin_dir="$HOME/.local/bin"
     chezmoi="$bin_dir/chezmoi"
     if [ "$(command -v curl)" ]; then
-      sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+      sh -c "$(curl -fsSL chezmoi.io/get)" -- -b "$bin_dir"
     elif [ "$(command -v wget)" ]; then
-      sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+      sh -c "$(wget -qO- chezmoi.io/get)" -- -b "$bin_dir"
     else
       echo "To install chezmoi, you must have curl or wget installed." >&2
       exit 1
