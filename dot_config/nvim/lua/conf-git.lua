e5d3ab6ed4581647b2679bad298b40473cef46e7
Fran Casas <nflamel@gmail.com>
2022-04-22 22:21:58 +0200

    fix(nvim): GBrowse on selected code

--- dot_config/nvim/lua/conf-git.lua
+++ dot_config/nvim/lua/conf-git.lua
@@ -4,4 +4,4 @@ vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
 vim.keymap.set("n", "<leader>gl", ":GV!<cr>") -- Log for current file
 vim.keymap.set("n", "<leader>gL", ":GV<cr>") -- Log for repo
 vim.keymap.set("n", "<leader>go", ":GBrowse<CR>")
-vim.keymap.set("n", "<leader>go", ":'<,'>GBrowse<CR>")
+vim.keymap.set("v", "<leader>go", ":'<,'>GBrowse<CR>")
