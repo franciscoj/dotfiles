8ab7f992e00f076a5ed8b238d48524aef0ed3dbd
Fran Casas <nflamel@gmail.com>
2022-06-10 13:53:57 +0200

    feat: set rdm open

--- dot_config/zplugins/codespaces/init.zsh
+++ dot_config/zplugins/codespaces/init.zsh
@@ -2,3 +2,8 @@
 if [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
+if [ -v CODESPACES ] ; then
+  alias xdg-open="rdm open"
+  gh config set browser "rdm open"
+fi
