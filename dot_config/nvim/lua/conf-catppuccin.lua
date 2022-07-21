5158b5d703d52a48e6ea01337577f455ec99fc1f
Fran Casas <nflamel@gmail.com>
2022-07-21 16:18:06 +0200

    feat(nvim): configure catppuccin support

--- dot_config/nvim/lua/conf-catppuccin.lua
+++ dot_config/nvim/lua/conf-catppuccin.lua
@@ -1,14 +1,26 @@
 local catppuccin = require("catppuccin")
 
 catppuccin.setup({
-	integrations = {
-		dashboard = false,
-		fern = true,
-		indent_blankline = { enabled = false },
-		lsp_saga = false,
-		lsp_trouble = true,
-		notify = false,
-		nvimtree = { enabled = false },
-	},
+  dim_inactive = {
+    enabled = true,
+    shade = "dark",
+  },
+  styles = {
+    comments = { "italic" },
+    functions = { "italic", "bold" },
+    keywords = { "bold" },
+    strings = { "italic" },
+  },
+  integrations = {
+    bufferline = true,
+    cmp = true,
+    fern = true,
+    gitsigns = true,
+    lsp_trouble = true,
+    markdown = true,
+    native_lsp = { enabled = true },
+    telescope = true,
+    treesitter = true,
+  },
 })
 vim.cmd([[colorscheme catppuccin]])
