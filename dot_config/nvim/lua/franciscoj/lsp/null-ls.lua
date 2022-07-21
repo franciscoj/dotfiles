82b023b5951ff0b9f0b46560548759fd15cbab62
Fran Casas <nflamel@gmail.com>
2022-07-21 16:17:48 +0200

    style(lua): format

--- dot_config/nvim/lua/franciscoj/lsp/null-ls.lua
+++ dot_config/nvim/lua/franciscoj/lsp/null-ls.lua
@@ -11,43 +11,43 @@ local sources = {
 }
 
 if features.golangci_lint then
-	table.insert(sources, diagnostics.golangci_lint)
+  table.insert(sources, diagnostics.golangci_lint)
 end
 
 if features.goimports then
-	table.insert(sources, formatting.goimports)
+  table.insert(sources, formatting.goimports)
 end
 
 if features.rubocop then
-	table.insert(sources, formatting.rubocop.with({ command = "bin/rubocop" }))
-	table.insert(sources, diagnostics.rubocop.with({ command = "bin/rubocop" }))
+  table.insert(sources, formatting.rubocop.with({ command = "bin/rubocop" }))
+  table.insert(sources, diagnostics.rubocop.with({ command = "bin/rubocop" }))
 end
 
 -- These are only relevant when editing DOTFILES
 if vim.env.DOTFILES then
-	if features.luacheck then
-		table.insert(sources, diagnostics.luacheck)
-	end
+  if features.luacheck then
+    table.insert(sources, diagnostics.luacheck)
+  end
 
-	if features.stylua then
-		table.insert(sources, formatting.stylua)
-	end
+  if features.stylua then
+    table.insert(sources, formatting.stylua)
+  end
 end
 
 if #sources ~= 0 then
-	null_ls.setup({
-		debug = false,
-		sources = sources,
-		on_attach = function(client)
-			if client.resolved_capabilities.document_formatting then
-				local id = vim.api.nvim_create_augroup("lsp_formatting", { clear = false })
-				vim.api.nvim_clear_autocmds({ buffer = 0, group = id })
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					buffer = 0,
-					group = id,
-					callback = vim.lsp.buf.formatting_sync,
-				})
-			end
-		end,
-	})
+  null_ls.setup({
+    debug = false,
+    sources = sources,
+    on_attach = function(client)
+      if client.resolved_capabilities.document_formatting then
+        local id = vim.api.nvim_create_augroup("lsp_formatting", { clear = false })
+        vim.api.nvim_clear_autocmds({ buffer = 0, group = id })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = 0,
+          group = id,
+          callback = vim.lsp.buf.formatting_sync,
+        })
+      end
+    end,
+  })
 end
