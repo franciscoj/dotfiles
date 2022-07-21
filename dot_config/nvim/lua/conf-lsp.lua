171e442c3ca84920c7817fecb886e769808ef70b
Fran Casas <nflamel@gmail.com>
2022-07-21 15:17:16 +0200

    feat(nvim): add lsp support for TypeScript

--- dot_config/nvim/lua/conf-lsp.lua
+++ dot_config/nvim/lua/conf-lsp.lua
@@ -1,7 +1,7 @@
 -- Make sure this is the very first so that it registers the installation
 -- hooks.
 require("nvim-lsp-installer").setup({
-	ensure_installed = { "sumneko_lua", "gopls", "yamlls" },
+	ensure_installed = { "sumneko_lua", "gopls", "yamlls", "tsserver" },
 })
 
 require("franciscoj.lsp.null-ls")
