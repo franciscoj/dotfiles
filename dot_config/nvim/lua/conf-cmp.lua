2b8735c1fd6d1d6e12bbdad30f817b53926ac702
Fran Casas <nflamel@gmail.com>
2022-09-21 21:14:50 +0200

    fix: use recommended cmp config

--- dot_config/nvim/lua/conf-cmp.lua
+++ dot_config/nvim/lua/conf-cmp.lua
@@ -36,14 +36,10 @@ return function()
       entries = { name = "custom", selection_order = "near_cursor" },
     },
     mapping = cmp.mapping.preset.insert({
-      ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      ["<C-e>"] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
@@ -54,7 +50,7 @@ return function()
         else
           fallback()
         end
-      end),
+      end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
@@ -63,7 +59,7 @@ return function()
         else
           fallback()
         end
-      end),
+      end, { "i", "s" }),
     }),
     sources = cmp.config.sources({
       { name = "nvim_lsp" },
