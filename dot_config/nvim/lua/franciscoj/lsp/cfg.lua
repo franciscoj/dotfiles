442b7c8c76cf8668ecf5927f89861ec21d94e48e
Fran Casas <nflamel@gmail.com>
2022-09-28 18:43:36 +0200

    style(nvim): autoformatter wants spaces now :shrug:

--- dot_config/nvim/lua/franciscoj/lsp/cfg.lua
+++ dot_config/nvim/lua/franciscoj/lsp/cfg.lua
@@ -3,74 +3,74 @@ local builtin = require("telescope.builtin")
 local themes = require("telescope.themes")
 
 local function on_attach(client, _bufnr)
-	-- Using LSP defaults
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-	vim.keymap.set("n", "<LocalLeader>t", vim.lsp.buf.type_definition)
-	vim.keymap.set("n", "<LocalLeader>f", vim.lsp.buf.formatting)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover)
-	vim.keymap.set("n", "<LocalLeader>k", vim.lsp.buf.signature_help)
-	vim.keymap.set("n", "<LocalLeader>r", vim.lsp.buf.rename)
-	vim.keymap.set("n", "<LocalLeader>;", function()
-		vim.diagnostic.open_float(nil, { focus = false, border = "rounded" })
-	end)
+  -- Using LSP defaults
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+  vim.keymap.set("n", "<LocalLeader>t", vim.lsp.buf.type_definition)
+  vim.keymap.set("n", "<LocalLeader>f", vim.lsp.buf.formatting)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover)
+  vim.keymap.set("n", "<LocalLeader>k", vim.lsp.buf.signature_help)
+  vim.keymap.set("n", "<LocalLeader>r", vim.lsp.buf.rename)
+  vim.keymap.set("n", "<LocalLeader>;", function()
+    vim.diagnostic.open_float(nil, { focus = false, border = "rounded" })
+  end)
 
-	-- I'm not sure why these are for
-	-- vim.keymap.set("n", "<LocalLeader>wa", vim.lsp.buf.add_workspace_folder)
-	-- vim.keymap.set("n", "<LocalLeader>wr", vim.lsp.buf.remove_workspace_folder)
-	-- vim.keymap.set("n", "<LocalLeader>wl", function()
-	-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	-- end)
+  -- I'm not sure why these are for
+  -- vim.keymap.set("n", "<LocalLeader>wa", vim.lsp.buf.add_workspace_folder)
+  -- vim.keymap.set("n", "<LocalLeader>wr", vim.lsp.buf.remove_workspace_folder)
+  -- vim.keymap.set("n", "<LocalLeader>wl", function()
+  -- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end)
 
-	-- Using Telescope
-	vim.keymap.set("n", "<LocalLeader>o", builtin.lsp_dynamic_workspace_symbols)
-	vim.keymap.set("n", "gr", builtin.lsp_references)
-	vim.keymap.set("n", "gi", builtin.lsp_implementations)
-	vim.keymap.set("n", "<LocalLeader>a", vim.lsp.buf.code_action)
-	vim.keymap.set("x", "<LocalLeader>a", vim.lsp.buf.range_code_action)
+  -- Using Telescope
+  vim.keymap.set("n", "<LocalLeader>o", builtin.lsp_dynamic_workspace_symbols)
+  vim.keymap.set("n", "gr", builtin.lsp_references)
+  vim.keymap.set("n", "gi", builtin.lsp_implementations)
+  vim.keymap.set("n", "<LocalLeader>a", vim.lsp.buf.code_action)
+  vim.keymap.set("x", "<LocalLeader>a", vim.lsp.buf.range_code_action)
 
-	-- Using trouble.nvim
-	vim.keymap.set("n", "<LocalLeader>d", "<cmd>Trouble document_diagnostics<CR>")
-	vim.keymap.set("n", "<LocalLeader>D", "<cmd>Trouble workspace_diagnostics<CR>")
+  -- Using trouble.nvim
+  vim.keymap.set("n", "<LocalLeader>d", "<cmd>Trouble document_diagnostics<CR>")
+  vim.keymap.set("n", "<LocalLeader>D", "<cmd>Trouble workspace_diagnostics<CR>")
 
-	-- Disable virtual diagnostics because they are mostly annoying
-	vim.diagnostic.config({ virtual_text = false })
+  -- Disable virtual diagnostics because they are mostly annoying
+  vim.diagnostic.config({ virtual_text = false })
 
-	if client.resolved_capabilities.code_lens then
-		local id = vim.api.nvim_create_augroup("lsp_code_lens_refresh", { clear = false })
-		vim.api.nvim_clear_autocmds({ buffer = 0, group = id })
-		vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "CursorHold" }, {
-			buffer = 0,
-			group = id,
-			callback = function()
-				vim.lsp.codelens.refresh()
-			end,
-		})
+  if client.resolved_capabilities.code_lens then
+    local id = vim.api.nvim_create_augroup("lsp_code_lens_refresh", { clear = false })
+    vim.api.nvim_clear_autocmds({ buffer = 0, group = id })
+    vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "CursorHold" }, {
+      buffer = 0,
+      group = id,
+      callback = function()
+        vim.lsp.codelens.refresh()
+      end,
+    })
 
-		vim.api.nvim_create_autocmd("BufEnter", {
-			buffer = 0,
-			callback = function()
-				vim.lsp.codelens.display()
-			end,
-			group = id,
-		})
+    vim.api.nvim_create_autocmd("BufEnter", {
+      buffer = 0,
+      callback = function()
+        vim.lsp.codelens.display()
+      end,
+      group = id,
+    })
 
-		vim.keymap.set("n", "<LocalLeader>A", vim.lsp.codelens.run)
-	end
+    vim.keymap.set("n", "<LocalLeader>A", vim.lsp.codelens.run)
+  end
 end
 
 local M = {}
 
 --- Returns the default configurations that are common to all LSPs
 M.defaults = function()
-	return {
-		capabilities = capabilities,
-		handlers = {
-			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-		},
-		on_attach = on_attach,
-	}
+  return {
+    capabilities = capabilities,
+    handlers = {
+      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+    },
+    on_attach = on_attach,
+  }
 end
 M.on_attach = on_attach
 
