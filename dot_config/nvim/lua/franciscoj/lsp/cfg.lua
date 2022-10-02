afc78da86dd907ce07b1806b08d042de53b56066
Fran Casas <nflamel@gmail.com>
2022-10-02 19:35:53 +0200

    style(nvim): remove unused import

--- dot_config/nvim/lua/franciscoj/lsp/cfg.lua
+++ dot_config/nvim/lua/franciscoj/lsp/cfg.lua
@@ -1,6 +1,5 @@
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local builtin = require("telescope.builtin")
-local themes = require("telescope.themes")
 
 local function on_attach(client, _bufnr)
 	-- Using LSP defaults
