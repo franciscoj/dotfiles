2f91938a1e36dfc0f28f2f58d5f8f37c865d6f68
Fran Casas <nflamel@gmail.com>
2022-10-20 09:51:51 +0200

    feat(snips): better ruby snippets

--- dot_config/nvim/lua/franciscoj/snippets/ruby.lua
+++ dot_config/nvim/lua/franciscoj/snippets/ruby.lua
@@ -18,16 +18,47 @@ local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.expand_conditions")
 
-local selection_or = function(txt)
+local selection_or = function(default)
 	return function(_args, snip)
 		local env = snip.env
-		local content = env.TM_SELECTED_TEXT[1] or txt
+		local selected = env.TM_SELECTED_TEXT
 
-		return sn(nil, { i(1, content) })
+		-- If the selection is empty, return the default text instead
+		if #selected == 0 then
+			return sn(nil, { i(1, default) })
+		end
+
+		-- If the selection has a single line as as the default of a new insert
+		-- node so that it can be edited or changed
+		if #selected == 1 then
+			return sn(nil, { i(1, selected[1]) })
+		end
+
+		-- If there's a multi line selection, then just add everything.
+		return sn(nil, { t(selected) })
 	end
 end
 
+local value_on = function(args, _snip)
+	return sn(nil, { i(1, args[1][1]) })
+end
+
 ls.add_snippets("ruby", {
+	-- Ruby general
+	s("@@", fmt("@{name} = {val}", { name = i(1, "name"), val = d(2, value_on, { 1 }) })),
+	s("=", fmt("{name} = {val}", { name = i(1, "name"), val = d(2, value_on, { 1 }) })),
+	s(
+		"mm",
+		fmt(
+			[[
+			module {name}
+			  {content}
+			end
+			]],
+			{ name = i(1, "Module"), content = d(2, selection_or("")) }
+		)
+	),
+	-- Sorbet specific
 	s("ty", {
 		t("# typed: "),
 		c(1, { t("true"), t("false"), t("strict") }),
