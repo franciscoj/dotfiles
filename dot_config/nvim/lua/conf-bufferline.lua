90bbbc78575bb7945e405e9251c3adbced5f7108
Fran Casas <nflamel@gmail.com>
2022-10-22 22:36:12 +0200

    feat(nvim): buffer close selector

--- dot_config/nvim/lua/conf-bufferline.lua
+++ dot_config/nvim/lua/conf-bufferline.lua
@@ -3,23 +3,24 @@ return function()
 	local bufferline = require("bufferline")
 	bufferline.setup({
 		options = {
+			separator_style = "thick",
 			diagnostics = "nvim_lsp",
 			show_buffer_close_icons = false,
 			show_close_icon = false,
-			diagnostics_indicator = function(_count, _level, diagnostics_dict, _context)
-				local diagnostics = " "
+			diagnostics_indicator = function(_count, _level, diagnostics, _context)
+				local txt = " "
 
-				for level, count in pairs(diagnostics_dict) do
+				for level, count in pairs(diagnostics) do
 					local symbol = level == "error" and " " or (level == "warning" and " " or " ")
 
-					diagnostics = diagnostics .. count .. symbol
+					txt = txt .. count .. symbol
 				end
 
-				return diagnostics
+				return txt
 			end,
 		},
 	})
 
 	h.nnoremap("<leader>b", bufferline.pick_buffer)
-	h.nnoremap("<leader>bd", "<cmd>:bdelete<cr>")
+	h.nnoremap("<leader>bd", "<cmd>BufferLinePickClose<cr>")
 end
