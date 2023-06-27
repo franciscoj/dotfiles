2f68ff33baa09b6fe6c6d22c83472488ce4c9d04
Fran Casas <nflamel@gmail.com>
2023-06-27 09:29:23 +0200

    fix: installation when running as root

--- install.sh
+++ install.sh
@@ -4,25 +4,30 @@ set -e # -e: exit on error
 
 if [ -z "${CODESPACES}" ]; then
   echo "not codespaces"
+  exit
+fi
+
+if [ "$EUID" -ne 0 ]
+  apt install -y ripgrep
 else
   sudo apt install -y ripgrep
+fi
 
-  if [ ! "$(command -v chezmoi)" ]; then
-    echo "Installing: chezmoi ********************"
+if [ ! "$(command -v chezmoi)" ]; then
+  echo "Installing: chezmoi ********************"
 
-    bin_dir="$HOME/.local/bin"
-    chezmoi="$bin_dir/chezmoi"
-    if [ "$(command -v curl)" ]; then
-      sh -c "$(curl -fsSL chezmoi.io/get)" -- -b "$bin_dir"
-    elif [ "$(command -v wget)" ]; then
-      sh -c "$(wget -qO- chezmoi.io/get)" -- -b "$bin_dir"
-    else
-      echo "To install chezmoi, you must have curl or wget installed." >&2
-      exit 1
-    fi
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL chezmoi.io/get)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- chezmoi.io/get)" -- -b "$bin_dir"
   else
-    chezmoi=chezmoi
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
   fi
+else
+  chezmoi=chezmoi
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
