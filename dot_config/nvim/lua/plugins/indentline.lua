643892451c8c9292a1dd61977dc0c507cc2b2c4a
Fran Casas <nflamel@gmail.com>
2023-09-16 18:23:37 +0200

    feat(nvim): add indent blankline

---
+++ dot_config/nvim/lua/plugins/indentline.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup({
+				space_char_blankline = " ",
+				show_current_context = true,
+			})
+		end,
+	},
+}
