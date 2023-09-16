87b966009527e5d9a8434f98587c5fa26440000c
Fran Casas <nflamel@gmail.com>
2023-09-16 17:53:41 +0200

    feat(nvim): let's try neotest

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -1,29 +1,58 @@
 return {
 	{
 		"vim-test/vim-test",
-		keys = {
-			{ "<leader>tf" },
-			{ "<leader>tl" },
-			{ "<leader>tr" },
-			{ "<leader>ts" },
-		},
 		config = function()
-			local h = require("h")
 			local fterm = function(cmd)
 				require("FTerm").run(cmd)
 			end
 
 			vim.g["test#custom_strategies"] = { fterm = fterm }
 			vim.g["test#strategy"] = "fterm"
-			vim.g["test#go#gotest#options"] = {
-				nearest = "-v",
-				file = "-v",
-			}
+		end,
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			-- File types
+			"haydenmeade/neotest-jest", -- JavaScript,TypeScript / Jest
+			"zidhuss/neotest-minitest", -- Ruby / minitest
+			"nvim-neotest/neotest-go", -- Go / go test - testify
+		},
+		keys = {
+			{ "<leader>tf" },
+			{ "<leader>tl" },
+			{ "<leader>tr" },
+			{ "<leader>tt" },
+			{ "<leader>to" },
+		},
+		config = function()
+			local h = require("h")
+			h.nnoremap("<leader>tf", function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end)
+			h.nnoremap("<leader>tl", function()
+				require("neotest").run.run()
+			end)
+			h.nnoremap("<leader>tr", function()
+				require("neotest").run.run_last()
+			end)
+			h.nnoremap("<leader>tt", function()
+				require("neotest").summary.toggle()
+			end)
+			h.nnoremap("<leader>to", function()
+				require("neotest").output_pannel.toggle()
+			end)
 
-			h.nnoremap("<leader>tf", ":TestFile<cr>")
-			h.nnoremap("<leader>tl", ":TestNearest<cr>")
-			h.nnoremap("<leader>tr", ":TestLast<cr>")
-			h.nnoremap("<leader>ts", ":TestSuite<cr>")
+			require("neotest").setup({
+				adapters = {
+					require("neotest-go"),
+					require("neotest-minitest"),
+					require("neotest-jest"),
+				},
+			})
 		end,
 	},
 }
