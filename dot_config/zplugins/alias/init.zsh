f7db724d4438c976c95d319047ee48038617f7c7
Fran Casas <nflamel@gmail.com>
2023-09-20 09:29:36 +0200

    refactor(shell): s/exa/eza

--- dot_config/zplugins/alias/init.zsh
+++ dot_config/zplugins/alias/init.zsh
@@ -6,14 +6,14 @@ alias rake="noglob rake"
 alias tmux="tmux -u"
 
 
-if ! command -v exa &> /dev/null
+if ! command -v eza &> /dev/null
 then
   alias ll="ls -lh"
   alias la="ls -lha"
 else
-  exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
-  alias ll="exa -lh ${exa_params}"
-  alias la="exa -lha ${exa_params}"
+  eza_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
+  alias ll="eza -lh ${eza_params}"
+  alias la="eza -lha ${eza_params}"
 fi
 
 alias be="bundle exec"
