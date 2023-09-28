a650b239af9f0ac18fe0156177eca4e5760cf052
Fran Casas <nflamel@gmail.com>
2023-09-28 10:26:13 +0200

    fix(nvim): indent blankline config

--- dot_config/nvim/lua/plugins/indentline.lua
+++ dot_config/nvim/lua/plugins/indentline.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			require("indent_blankline").setup({
+			require("ibl").setup({
 				space_char_blankline = " ",
 				show_current_context = true,
 			})
