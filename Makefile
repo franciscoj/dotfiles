abfdd602b0de6d1317ddb4f6677fd571360fa154
Fran Casas <nflamel@gmail.com>
2023-10-02 09:59:22 +0200

    feat(nvim): add alias for OverseerRun

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: apply
+
 install:
 	./install.sh
 
--- dot_config/nvim/lua/plugins/overseer.lua
+++ dot_config/nvim/lua/plugins/overseer.lua
@@ -1,7 +1,13 @@
 return {
 	{
 		"stevearc/overseer.nvim",
+		keys = {
+			{ "<leader>T" },
+		},
 		config = function()
+			local h = require("h")
+
+			h.nnoremap("<leader>T", "<CMD>OverseerRun<CR>")
 			require("overseer").setup({
 				strategy = "toggleterm",
 			})
