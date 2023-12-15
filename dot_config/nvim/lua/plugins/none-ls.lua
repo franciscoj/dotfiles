3d196c8e59bea0f4c2ef20301e0c66c32d6204cd
Fran Casas <nflamel@gmail.com>
2023-12-15 11:13:12 +0100

    feat(nvim): lint with null-ls

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -2,8 +2,8 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-			local actions = null_ls.builtins.code_actions
-		-- local diagnostics = null_ls.builtins.diagnostics
+		local actions = null_ls.builtins.code_actions
+		local diagnostics = null_ls.builtins.diagnostics
 		local Config = require("franciscoj.lsp.config")
 		local formatting = null_ls.builtins.formatting
 		local features = require("franciscoj.lsp.features")
@@ -12,21 +12,29 @@ return {
 		local ensure = {
 			{ name = "stylua" },
 			{ name = "prettier" },
+			{ name = "luacheck" },
 		}
+
 		if features.go then
 			table.insert(ensure, { name = "goimports", version = "latest" })
+			table.insert(ensure, { name = "golangci-lint", version = "v1.53.3" })
 		end
+
 		mason.ensure_tools(ensure)
 
 		local cfg = Config:new({
 			sources = {
+				actions.gitsigns,
 				actions.gomodifytags,
 				actions.impl,
-				actions.gitsigns,
+				diagnostics.eslint,
+				diagnostics.golangci_lint,
+				diagnostics.luacheck,
+				diagnostics.rubocop.with({ timeout = 5000 }),
 				formatting.goimports,
 				formatting.prettier,
+				formatting.rubocop.with({ timeout = 5000 }),
 				formatting.stylua,
-				formatting.rubocop,
 				formatting.trim_newlines,
 				formatting.trim_whitespace,
 			},
