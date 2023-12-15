f2569b76132a1abb1cc4f8c756bddddf2ec9ee95
Fran Casas <nflamel@gmail.com>
2023-12-15 10:58:41 +0100

    feat(nvim): missing overseer shortcuts

--- dot_config/nvim/lua/plugins/overseer.lua
+++ dot_config/nvim/lua/plugins/overseer.lua
@@ -3,11 +3,13 @@ return {
 		"stevearc/overseer.nvim",
 		keys = {
 			{ "<leader>T" },
+			{ "<leader>O" },
 		},
 		config = function()
 			local h = require("h")
 
 			h.nnoremap("<leader>T", "<CMD>OverseerRun<CR>")
+			h.nnoremap("<leader>O", "<CMD>OverseerToggle<CR>")
 			require("overseer").setup({
 				strategy = "toggleterm",
 			})
