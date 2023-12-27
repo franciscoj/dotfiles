83a146b353f419230d4a2785e033db4998a350fa
Fran Casas <nflamel@gmail.com>
2023-12-27 18:32:31 +0100

    feat(install): update for ubuntu server

---
+++ dot_config/nvim/lua/franciscoj/lsp/configs/rust_analyzer.lua
@@ -0,0 +1,21 @@
+local lspconfig = require("lspconfig")
+local Config = require("franciscoj.lsp.config")
+local features = require("franciscoj.lsp.features")
+
+if features.rust ~= true then
+	return
+end
+
+local cfg = Config:new({
+	settings = {
+		-- to enable rust-analyzer settings visit:
+		-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+		["rust-analyzer"] = {
+			checkOnSave = {
+				command = "clippy",
+			},
+		},
+	},
+})
+
+lspconfig.rust_analyzer.setup(cfg:to_lspconfig())
