f1b56df110130d2b58e7459533f3f1f690d7ef71
Fran Casas <nflamel@gmail.com>
2024-04-11 15:52:15 +0200

    refactor(nvim): improve fzf shortcuts

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -4,11 +4,14 @@ return {
 		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
+			{ "<leader>fG" },
+			{ "<leader>fb" },
 			{ "<leader>ff" },
 			{ "<leader>fg" },
-			{ "<leader>fG" },
-			{ "<leader>gf" },
 			{ "<leader>fm" },
+			{ "<leader>fr" },
+			{ "<leader>gf" },
+			{ "i", "<localleader>'" },
 		},
 		config = function()
 			local fzf = require("fzf-lua")
@@ -29,15 +32,18 @@ return {
 						["ctrl-l"] = { actions.grep_lgrep },
 						["ctrl-g"] = false,
 					},
-				}
+				},
 			})
 			-- calling `setup` is optional for customization
 
+			vim.keymap.set("n", "<leader>fG", fzf.grep)
+			vim.keymap.set("n", "<leader>fb", fzf.buffers)
 			vim.keymap.set("n", "<leader>ff", fzf.files)
 			vim.keymap.set("n", "<leader>fg", fzf.live_grep)
-			vim.keymap.set("n", "<leader>fG", fzf.grep)
-			vim.keymap.set("n", "<leader>gf", fzf.git_status)
 			vim.keymap.set("n", "<leader>fm", fzf.marks)
+			vim.keymap.set("n", "<leader>fr", fzf.registers)
+			vim.keymap.set("n", "<leader>gf", fzf.git_status)
+			vim.keymap.set("i", "<localleader>'", fzf.registers)
 		end,
 	},
 }
