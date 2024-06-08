bb87c024e16160a833982ee43f87e6f879e27779
Fran Casas <nflamel@gmail.com>
2024-06-08 14:23:05 +0200

    feat(nvim): try a new autocomplete setup
    This one looks like it is a bit less invasive and has less visual load.

--- dot_config/nvim/lua/plugins/autocomplete.lua
+++ dot_config/nvim/lua/plugins/autocomplete.lua
@@ -7,7 +7,6 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
-			"onsails/lspkind-nvim",
 			-- snippets
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
@@ -16,29 +15,11 @@ return {
 		event = "InsertEnter",
 		config = function()
 			local cmp = require("cmp")
-			local lspkind = require("lspkind")
 			local luasnip = require("luasnip")
 
 			require("franciscoj.snippets")
 
-			local has_words_before = function()
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
-
 			cmp.setup({
-				formatting = {
-					fields = { "abbr", "kind", "menu" },
-					format = lspkind.cmp_format({
-						mode = "symbol",
-						menu = {
-							buffer = "buf",
-							nvim_lsp = "lsp",
-							luasnip = "snip",
-						},
-					}),
-				},
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
@@ -48,30 +29,15 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
-				view = {
-					entries = { name = "custom", selection_order = "near_cursor" },
-				},
 				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-y>"] = cmp.mapping(cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					})),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							-- Select the first entry if it is the only one.
-							if #cmp.get_entries() == 1 then
-								cmp.confirm({ select = true })
-							else
-								cmp.select_next_item()
-							end
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						elseif has_words_before() then
-							cmp.complete()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
 					["<C-l>"] = cmp.mapping(function(fallback)
 						if luasnip.jumpable(1) then
 							luasnip.jump(1)
@@ -86,21 +52,12 @@ return {
 							fallback()
 						end
 					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
 					{ name = "nvim_lsp_signature_help" },
-				}, {
+					{ name = "luasnip" },
+					{ name = "path" },
 					{ name = "buffer", keyword_length = 4 },
 				}),
 			})
@@ -108,16 +65,15 @@ return {
 			cmp.setup.cmdline({ "/", "?" }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-					{ name = "buffer", keyword_length = 4 },
+					{ name = "buffer", keyword_length = 6 },
 				},
 			})
 
 			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
+					{ name = "path", keyword_length = 4 },
+					{ name = "cmdline", keyword_length = 4 },
 				}),
 			})
 		end,
