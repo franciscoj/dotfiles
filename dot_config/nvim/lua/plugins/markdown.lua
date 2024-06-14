2fd660ac5c1d43211733f89875b2ba08c4dfb910
Fran Casas <nflamel@gmail.com>
2024-06-14 11:32:32 +0200

    fix(nvim): markdown preview

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -3,8 +3,12 @@ local features = require("franciscoj.lsp.features")
 return {
 	{
 		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		enabled = not vim.g.started_by_firenvim and not features.codespaces,
-		ft = { "markdown" },
 		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	},
 }
