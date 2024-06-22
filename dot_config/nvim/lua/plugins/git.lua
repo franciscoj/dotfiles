82d442ee2a9f9d1ca3629e5ad99b528579e1a0d2
Fran Casas <nflamel@gmail.com>
2024-06-22 17:27:01 +0200

    fix(nvim): lazy.nvim warnings

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -51,7 +51,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope.nvim",
-			"kyazdani42/nvim-web-devicons",
+			"nvim-tree/nvim-web-devicons",
 		},
 		cmd = { "Octo" },
 		config = function()
