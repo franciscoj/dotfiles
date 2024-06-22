41cecc6f6e39f5b1c6f542c3f584ac22414fa229
Fran Casas <nflamel@gmail.com>
2024-06-22 17:04:20 +0200

    feat(nvim): test toggleterm on a tab instead

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -11,16 +11,7 @@ return {
 		},
 		config = function()
 			require("toggleterm").setup({
-				direction = "float",
-				float_opts = {
-					border = "rounded",
-					width = function()
-						return math.floor(vim.o.columns * 0.95)
-					end,
-					height = function()
-						return math.floor(vim.o.lines * 0.95)
-					end,
-				},
+				direction = "tab",
 			})
 		end,
 	},
