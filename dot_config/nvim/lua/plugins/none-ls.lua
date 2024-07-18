481384a1a30aac013cba7ada4265634a7303cc8b
Fran Casas <nflamel@gmail.com>
2024-07-18 14:09:34 +0200

    refactor(nvim): reorganize none-ls config
    Simplify a bit, less conditions

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -24,37 +24,26 @@ return {
 
 		-- These will be automatically installed through mason
 		local ensure = {}
+		local sources = {}
 
 		if features.typescript then
 			table.insert(ensure, { name = "prettier" })
+
+			table.insert(sources, formatting.prettier)
 		end
 
 		if features.lua then
 			table.insert(ensure, { name = "stylua" })
 			table.insert(ensure, { name = "selene" })
-		end
 
-		if features.go then
-			table.insert(ensure, { name = "goimports", version = "latest" })
-			table.insert(ensure, { name = "golangci-lint", version = "v1.53.3" })
-		end
-
-		mason.ensure_tools(ensure)
-
-		local sources = {
-			actions.gitsigns,
-		}
-
-		if features.typescript then
-			table.insert(sources, formatting.prettier)
-		end
-
-		if features.lua then
 			table.insert(sources, formatting.stylua)
 			table.insert(sources, diagnostics.selene)
 		end
 
 		if features.go then
+			table.insert(ensure, { name = "goimports", version = "latest" })
+			table.insert(ensure, { name = "golangci-lint", version = "v1.53.3" })
+
 			table.insert(sources, actions.gomodifytags)
 			table.insert(sources, actions.impl)
 			table.insert(sources, formatting.goimports)
@@ -69,6 +58,8 @@ return {
 			table.insert(sources, diagnostics.rubocop.with({ timeout = 5000, prefer_local = "bin" }))
 			table.insert(sources, formatting.rubocop.with({ timeout = 5000, prefer_local = "bin" }))
 		end
+
+		mason.ensure_tools(ensure)
 
 		local cfg = Config:new({ sources = sources })
 		null_ls.setup(cfg:to_lspconfig())
