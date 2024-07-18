ea667c722c60bc4da86c7aafd2975bc04400aa00
Fran Casas <nflamel@gmail.com>
2024-07-18 14:09:13 +0200

    feat(nvim): add removing session command

--- dot_config/nvim/lua/plugins/session.lua
+++ dot_config/nvim/lua/plugins/session.lua
@@ -3,6 +3,9 @@ return {
 	{
 		"tpope/vim-obsession",
 		enabled = not vim.g.started_by_firenvim,
-		keys = { { "<leader>ss", "<cmd>Obsess<cr>" } },
+		keys = {
+			{ "<leader>ss", "<cmd>Obsession<cr>" },
+			{ "<leader>sd", "<cmd>Obsession!<cr>" },
+		},
 	},
 }
