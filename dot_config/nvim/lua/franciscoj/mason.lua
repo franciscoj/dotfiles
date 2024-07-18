1ff66205dedb46d8bd03ffce6eef3a6b32e6aee9
Fran Casas <nflamel@gmail.com>
2024-07-18 14:08:42 +0200

    refactor(nvim): remove unused function

--- dot_config/nvim/lua/franciscoj/mason.lua
+++ dot_config/nvim/lua/franciscoj/mason.lua
@@ -1,18 +1,12 @@
-local reg = require("mason-registry")
-local path = require("mason-core.path")
+local registry = require("mason-registry")
 
 local M = {}
-
--- receives a package name and returns the path to its binary
-M.get_path = function(package_name)
-	return path.concat({ vim.fn.stdpath("data"), "mason", "bin", package_name })
-end
 
 -- Receives a table of { name = "package", version = "v1.1.1" } and makes sure they are all installed.
 -- `version` can be ommited
 M.ensure_tools = function(packages)
 	for _, package in ipairs(packages) do
-		local p = reg.get_package(package["name"])
+		local p = registry.get_package(package["name"])
 
 		if not p:is_installed() then
 			p:install({ version = package["version"] })
