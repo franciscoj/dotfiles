e9c3b4e545bc995fe229e11335c41a93ec7c32fa
Fran Casas <nflamel@gmail.com>
2025-06-11 15:43:23 +0100

    fix(nvim): omnisharp stuff

--- dot_config/nvim/lua/franciscoj/lsp/configs/omnisharp.lua
+++ dot_config/nvim/lua/franciscoj/lsp/configs/omnisharp.lua
@@ -1,6 +1,8 @@
 local lspconfig = require("lspconfig")
 local Config = require("franciscoj.lsp.config")
 
-local cfg = Config:new({})
+local cfg = Config:new({
+	cmd = { "OmniSharp" },
+})
 
 lspconfig.omnisharp.setup(cfg:to_lspconfig())
