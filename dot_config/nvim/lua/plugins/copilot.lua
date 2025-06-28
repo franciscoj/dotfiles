cf31014ff2415ead539d67ffb748a90e4cb0e6ea
Fran Casas <nflamel@gmail.com>
2025-06-28 22:27:36 +0200

    feat(nvim): add CodeCompanion
    And also mcphub

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -63,6 +63,14 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 		build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-		config = function() require("mcphub").setup() end,
+		config = function()
+			require("mcphub").setup({
+				log = {
+					level = vim.log.levels.DEBUG,
+					to_file = true,
+					file_path = vim.fn.expand("~/mcphub.log"),
+				},
+			})
+		end,
 	},
 }
