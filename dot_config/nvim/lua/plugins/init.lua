0bbeb6c347ed710e2d025c2942b43d29b3a67e9b
Fran Casas <nflamel@gmail.com>
2025-07-12 09:38:23 +0200

    chore(nvim): add folke/snacks.nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -2,6 +2,39 @@
 return {
 	"folke/lazy.nvim",
 	-- QOL plugins
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			bigfile = { enabled = true },
+			debug = { enabled = true },
+			indent = {
+				enabled = true,
+				only_scope = true,
+				only_current = true,
+			},
+			toggle = { enabled = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...) Snacks.debug.inspect(...) end
+					_G.bt = function() Snacks.debug.backtrace() end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+					-- Create some toggle mappings
+					Snacks.toggle.option("wrap", { name = "Wrap" }):map("yow")
+					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("yor")
+					Snacks.toggle.diagnostics():map("yod")
+					Snacks.toggle.line_number():map("yon")
+				end,
+			})
+		end,
+	},
 	{ "nvim-lua/plenary.nvim" },
 	{
 		"numToStr/Comment.nvim",
@@ -13,12 +46,6 @@ return {
 		version = "*",
 		event = { "VeryLazy" },
 		config = function() require("mini.surround").setup() end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-		version = "*",
-		event = { "VeryLazy" },
-		config = function() require("mini.indentscope").setup() end,
 	},
 	{
 		"echasnovski/mini.pairs",
