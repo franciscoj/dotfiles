c8193367442d05c2cf93e2ef71f163191772fdf7
Fran Casas <nflamel@gmail.com>
2025-07-12 12:30:50 +0200

    fix(nvim): lua_ls config

--- dot_config/nvim/lua/franciscoj/lsp/configs/lua_ls.lua
+++ dot_config/nvim/lua/franciscoj/lsp/configs/lua_ls.lua
@@ -3,13 +3,12 @@ lazydev.setup()
 
 vim.lsp.config("lua_ls", {
 	root_dir = function(bufnr, on_dir) on_dir(lazydev.find_workspace(bufnr)) end,
-	handlers = {
-		["textDocument/publishDiagnostics"] = function() end,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "Snacks" },
+			},
+		},
 	},
-	on_attach = function(client)
-		-- Disable the default formatting capabilities of the lua LSP
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentRangeFormattingProvider = false
-	end,
 })
 vim.lsp.enable("lua_ls")
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -21,11 +21,6 @@ return {
 			vim.api.nvim_create_autocmd("User", {
 				pattern = "VeryLazy",
 				callback = function()
-					-- Setup some globals for debugging (lazy-loaded)
-					_G.dd = function(...) Snacks.debug.inspect(...) end
-					_G.bt = function() Snacks.debug.backtrace() end
-					vim.print = _G.dd -- Override print to use snacks for `:=` command
-
 					-- Create some toggle mappings
 					Snacks.toggle.option("wrap", { name = "Wrap" }):map("yow")
 					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("yor")
