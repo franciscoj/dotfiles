38e2c5caf0d7a9823f9f5af870a07a9a8d2b8199
Fran Casas <nflamel@gmail.com>
2025-08-01 22:59:23 +0200

    feat(nvim): use avante instead of code companion

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,76 +1,38 @@
 --# selene: allow(mixed_table)
 return {
 	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
+		"yetone/avante.nvim",
+		build = "make",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		---@module 'avante'
+		---@type avante.Config
 		opts = {
-			suggestion = { enabled = false },
-			panel = { enabled = false },
-		},
-	},
-	{
-		"olimorris/codecompanion.nvim",
-		keys = {
-			{ "<leader>C", "<cmd>CodeCompanionChat Toggle<cr>" },
-			{ "<localleader>C", "<cmd>CodeCompanion<cr>" },
-		},
-		opts = {
-			display = {
-				chat = {
-					intro_message = "",
-				},
-			},
-			strategies = {
-				chat = { adapter = "copilot" },
-				inline = { adapter = "copilot" },
-				cmd = { adapter = "copilot" },
-			},
-			extensions = {
-				mcphub = {
-					callback = "mcphub.extensions.codecompanion",
-					opts = {
-						make_vars = true,
-						make_slash_commands = true,
-						show_result_in_chat = true,
-					},
-				},
+			-- add any opts here
+			-- for example
+			provider = "copilot",
+			auto_suggestions_provider = "copilot",
+			suggestion = {
+				debounce = 1800,
 			},
 		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"ravitemer/mcphub.nvim",
+			"MunifTanjim/nui.nvim",
+			"hrsh7th/nvim-cmp",   -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua",   -- for file_selector provider fzf
+			"stevearc/dressing.nvim", -- for input provider dressing
+			"folke/snacks.nvim",  -- for input provider snacks
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
 		},
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-		opts = {
-			file_types = { "markdown", "codecompanion" },
-		},
-	},
-	{
-		"echasnovski/mini.diff",
-		config = function()
-			local diff = require("mini.diff")
-			diff.setup({ source = diff.gen_source.none() })
-		end,
-	},
-	{
-		"ravitemer/mcphub.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-		config = function()
-			require("mcphub").setup({
-				-- log = {
-				-- 	level = vim.log.levels.DEBUG,
-				-- 	to_file = true,
-				-- 	file_path = vim.fn.expand("~/mcphub.log"),
-				-- },
-			})
-		end,
 	},
 }
