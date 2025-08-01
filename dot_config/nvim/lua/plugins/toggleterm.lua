d45cac8434e63461d282d22590f0a56efa43dea4
Fran Casas <nflamel@gmail.com>
2025-08-01 22:57:35 +0200

    refactor(nvim): simplify toggleterm config

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -9,11 +9,6 @@ return {
 			{ "<leader><ESC>", [[<CMD>:ToggleTerm<CR>]], desc = "Toggle terminal" },
 			{ "<leader><ESC>", [[<C-\><C-N><CMD>:ToggleTerm<CR>]], mode = "t", desc = "Toggle terminal" },
 			{ "<LocalLeader><ESC>", [[<C-\><C-N>]], mode = "t", desc = "Normal mode" },
-		},
-		config = function()
-			require("toggleterm").setup({
-				direction = "tab",
-			})
-		end,
+		}
 	},
 }
