168fc65b70e9bb8704f80db5e5927a07f705b0c3
Fran Casas <nflamel@gmail.com>
2025-08-02 14:23:48 +0200

    refactor(nvim): simplify go plugin conf

--- dot_config/nvim/lua/plugins/go.lua
+++ dot_config/nvim/lua/plugins/go.lua
@@ -7,7 +7,7 @@ return {
 			"neovim/nvim-lspconfig",
 			"nvim-treesitter/nvim-treesitter",
 		},
-		config = function() require("go").setup() end,
+		opt = {},
 		event = { "CmdlineEnter" },
 		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	},
