477cc257617bfb52ae30361352e143b7b832173d
Fran Casas <nflamel@gmail.com>
2025-08-05 11:01:12 +0200

    chore(nvim): upgrade fzf lua config
    Latest upgrade have added some nice new features

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -5,13 +5,17 @@ return {
 		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
-			{ "<leader>fG", function() require("fzf-lua").grep() end, desc = "Grep" },
-			{ "<leader>fb", function() require("fzf-lua").buffers() end, desc = "Find Buffer" },
-			{ "<leader>ff", function() require("fzf-lua").files() end, desc = "Find File" },
+			{ "<leader>fG", function() require("fzf-lua").grep() end,      desc = "Grep" },
+			{ "<leader>ff", function() require("fzf-lua").global() end,    desc = "Global Find" },
 			{ "<leader>fg", function() require("fzf-lua").live_grep() end, desc = "Live Grep" },
-			{ "<leader>fm", function() require("fzf-lua").marks() end, desc = "Find Mark" },
+			{ "<leader>fm", function() require("fzf-lua").marks() end,     desc = "Find Mark" },
 			{ "<leader>fr", function() require("fzf-lua").registers() end, desc = "Find Registers" },
-			{ "<leader>fv", function() require("fzf-lua").grep_visual() end, desc = "Find Visual Selection", mode = { "x" } },
+			{
+				"<leader>fv",
+				function() require("fzf-lua").grep_visual() end,
+				desc = "Find Visual Selection",
+				mode = { "x" },
+			},
 			{ "<leader>fw", function() require("fzf-lua").grep_cword() end, desc = "Find Word Under Cursor" },
 		},
 		config = function()
@@ -20,8 +24,14 @@ return {
 			fzf.register_ui_select()
 			fzf.setup({
 				winopts = {
-					height = 0.99,
-					width = 0.99,
+					height = 0.8,
+					width = 0.8,
+					backdrop = 70,
+					preview = {
+						vertical = "down:70%",
+						layout = "flex",
+						flip_columns = 190,
+					},
 				},
 				files = {
 					actions = {
