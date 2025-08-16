777833ecf366113f6ad1f86312b7ec8cfbfad0ec
Fran Casas <nflamel@gmail.com>
2025-08-16 17:34:41 +0200

    refactor(nvim): simplify bufferline

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -11,17 +11,6 @@ return {
 					diagnostics = "nvim_lsp",
 					show_buffer_close_icons = false,
 					show_close_icon = false,
-					diagnostics_indicator = function(_count, _level, diagnostics, _context)
-						local txt = " "
-
-						for level, count in pairs(diagnostics) do
-							local symbol = level == "error" and " " or (level == "warning" and " " or " ")
-
-							txt = txt .. count .. symbol
-						end
-
-						return txt
-					end,
 				},
 			})
 
