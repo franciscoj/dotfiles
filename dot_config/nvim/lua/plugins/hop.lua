ac52678efb455a3cc72cb8788c97c54f1c7ea154
Fran Casas <nflamel@gmail.com>
2025-08-27 11:40:36 +0200

    refactor(nvim): put flash behind localleader
    Default conf remaps `s` and `S`, which I use and I find it a bit
    annoying.

--- dot_config/nvim/lua/plugins/hop.lua
+++ dot_config/nvim/lua/plugins/hop.lua
@@ -7,11 +7,11 @@ return {
 		opts = {},
 		-- stylua: ignore
 		keys = {
-			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-			{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-			{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+			{ "<localleader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "<localleader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+			{ "<localleader>r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "<localleader>R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>",          mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
 		},
 	},
 }
