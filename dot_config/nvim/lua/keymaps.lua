cc67375342c549b762f9829bd6c88069fbbfbdfb
Fran Casas <nflamel@gmail.com>
2025-08-27 11:41:59 +0200

    refactor(nvim): refactor/cleanup general keymaps
    Just a bit of cleanup, use `<cmd>` instead of `:` for commands and also
    improve messages/notifications a bit.

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -5,30 +5,32 @@ vim.keymap.set("n", "<leader>;", "mzA;<esc>`z:delm z<cr>", { desc = "EOL ;" })
 vim.keymap.set("n", "<leader>:", "mzA:<esc>`z:delm z<cr>", { desc = "EOL :" })
 
 -- Save
-vim.keymap.set("n", "<leader>w", ":write<cr>", { desc = "Write file" })
-vim.keymap.set("n", "<leader>wa", ":wall<cr>", { desc = "Write all files" })
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Write file" })
+vim.keymap.set("n", "<leader>wa", "<cmd>wall<cr>", { desc = "Write all files" })
 
 -- Edit file on current folder
 vim.keymap.set("n", "<leader>e", ":e <C-R>=expand('%:p:h') . '/'<cr>", { desc = "Edit on current folder" })
 vim.keymap.set("n", "<leader>ec", [[:let @+ = expand("%:p:h")<cr>]], { desc = "Copy current path" })
-vim.keymap.set("n", "<leader>eC", [[:let @+ = expand("%:p")<cr>]], { desc = "Copy current absolute path" })
 vim.keymap.set("n", "<leader>ef", [[:let @+ = expand("%:p:t")<cr>]], { desc = "Copy current file name" })
+
+
+
 
 -- Toggle search highlight off
 vim.keymap.set("n", "<LocalLeader><esc>", ":nohlsearch<cr>", { desc = "Hide highlights" })
 
 -- Win management
-vim.keymap.set("n", "<C-J>", ":wincmd j<CR>", { desc = "Window ->" })
-vim.keymap.set("n", "<C-K>", ":wincmd k<CR>", { desc = "Window ↓" })
-vim.keymap.set("n", "<C-H>", ":wincmd h<CR>", { desc = "Window <-" })
-vim.keymap.set("n", "<C-L>", ":wincmd l<CR>", { desc = "Window ↑" })
-vim.keymap.set("n", "<leader>s", ":wincmd s<CR>", { desc = "Split --" })
-vim.keymap.set("n", "<leader>v", ":wincmd v<CR>", { desc = "Split |" })
-vim.keymap.set("n", "<leader>q", ":wincmd c<CR>", { desc = "Close window" })
+vim.keymap.set("n", "<C-J>", "<cmd>wincmd j<CR>", { desc = "Window ->" })
+vim.keymap.set("n", "<C-K>", "<cmd>wincmd k<CR>", { desc = "Window ↓" })
+vim.keymap.set("n", "<C-H>", "<cmd>wincmd h<CR>", { desc = "Window <-" })
+vim.keymap.set("n", "<C-L>", "<cmd>wincmd l<CR>", { desc = "Window ↑" })
+vim.keymap.set("n", "<leader>s", "<cmd>wincmd s<CR>", { desc = "Split --" })
+vim.keymap.set("n", "<leader>v", "<cmd>wincmd v<CR>", { desc = "Split |" })
+vim.keymap.set("n", "<leader>q", "<cmd>wincmd c<CR>", { desc = "Close window" })
 
 -- Tab management
-vim.keymap.set("n", "<leader>tt", ":tabnew %<CR>", { desc = "New tab" })
-vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab" })
+vim.keymap.set("n", "<leader>tt", "<C-W>T", { desc = "Move to new tab" })
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close tab" })
 
 -- Code movement
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -53,9 +55,9 @@ vim.keymap.set("n", "<leader>sl", function()
 		vim.cmd([[source Session.vim]])
 		vim.notify("Session loaded", vim.log.levels.INFO)
 	else
-		vim.notify("No Session.vim file", vim.log.levels.WARN)
+		vim.notify("No Session.vim file", vim.log.levels.ERROR)
 	end
-end, { desc = "Load session" })
+end, { desc = "Load existing session" })
 
 -- Open the repo name inside quotes on the browser
 vim.keymap.set("n", "<leader>og", function()
