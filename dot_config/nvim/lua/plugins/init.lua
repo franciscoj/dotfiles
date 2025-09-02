2fd5a82f821103055cee5f0fe6428b6c28869975
Fran Casas <nflamel@gmail.com>
2025-09-02 17:04:57 +0200

    fix(nvim): disable image unless ueberzug is installed

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -127,6 +127,9 @@ return {
 	{
 		"3rd/image.nvim",
 		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		-- I don't use this everywhere, only where I can use ueberzug                                                                                                         │   30 │   │   │   │   │   │   .new({
+		enabled = vim.fn.executable("ueberzug") == 1,
+		id = "image",
 		opts = {
 			backend = "ueberzug",
 			processor = "magick_cli",
