d8bb6ca47fedc10e349ca5884217008706083ddd
Fran Casas <nflamel@gmail.com>
2025-09-02 17:51:57 +0200

    fix(nvim): mcphub can't be loaded with `opts` :(

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -73,16 +73,18 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 		build = "npm install -g mcp-hub@latest",
-		opts = {
-			json_decode = parse_json,
-			extensions = {
-				copilotchat = {
-					enabled = true,
-					convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
-					convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
-					add_mcp_prefix = false, -- Add "mcp_" prefix to function names
+		config = function()
+			require("mcphub").setup({
+				json_decode = parse_json,
+				extensions = {
+					copilotchat = {
+						enabled = true,
+						convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
+						convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+						add_mcp_prefix = false, -- Add "mcp_" prefix to function names
+					},
 				},
-			},
-		},
+			})
+		end,
 	},
 }
